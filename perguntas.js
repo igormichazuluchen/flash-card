1d895c8ad3b9b5fde50562ca40d37024bac2faa2
igormichazuluchen <148573097+igormichazuluchen@users.noreply.github.com>
2024-11-18 09:20:34 -0300

    xfgn

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Lua?',
+    'Lua é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Finlândia?',
+    'A capital da Finlândia é Helsinque'
+)
+
+criaCartao(
+    'Culinária',
+    'Quais são os pratos típicos da Itália?',
+    'Os pratos mais típicos da Itália são: Pizza,Ravioli, Spaghetti, Lasagna, Risotto e Arancino'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz romã em Inglês?',
+    'Romã em ingles é pomegranate'
+)
